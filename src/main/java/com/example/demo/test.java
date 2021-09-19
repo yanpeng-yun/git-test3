4ae25d21943e2f47604c876d686ef57e62e70ffc
yanpengrong <2965038645@qq.com>
2021-09-19 15:10:22 +0800

    master-commit-test2

--- src/main/java/com/example/demo/test.java
+++ src/main/java/com/example/demo/test.java
@@ -6,5 +6,6 @@ public class test {
         System.out.println("wocao");
         System.out.println("wocao");
         System.out.println("wocao");
+        System.out.println("master更改了");
     }
 }
